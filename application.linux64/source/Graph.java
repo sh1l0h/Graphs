42cfb6fee94064c67b524ed906e02cc70ea6d154
kiu_gachechiladze.goga <gachechiladze.goga@kiu.edu.ge>
2021-11-23 15:48:55 +0400

    print matrix

--- application.linux64/source/Graph.java
+++ application.linux64/source/Graph.java
@@ -1,18 +1,6 @@
 import processing.core.*; 
-import processing.data.*; 
-import processing.event.*; 
-import processing.opengl.*; 
 
-import java.util.Scanner; 
-
-import java.util.HashMap; 
 import java.util.ArrayList; 
-import java.io.File; 
-import java.io.BufferedReader; 
-import java.io.PrintWriter; 
-import java.io.InputStream; 
-import java.io.OutputStream; 
-import java.io.IOException; 
 
 public class Graph extends PApplet {
 
@@ -29,6 +17,10 @@ class Node {
     this.x = x;
     this.y = y;
   }
+  public int getId() {
+	  return this.id;
+  }
+  
 
 }
 
@@ -40,24 +32,37 @@ class Edge {
   public Edge(Node from, Node to){
     this.from = from;
     this.to = to;
+    
   }
   
   public boolean equals(Edge a){
     return (a.from == from && a.to == to )||(a.from == to && a.to == from);
   }
+  public Node getFrom() {
+	  return this.from;
+  }
+  public Node getTo() {
+	  return this.to;
+  }
+  public String toString() {
+	  return (from.id+1)+" -> "+(to.id+1);
+  }
+  
 }
 
 
 
-int[][] graph;
+int[][] graph = new int[0][0];
 ArrayList<Node> nodes;
 ArrayList<Edge> edges;
+ArrayList<Integer> matrix;
 
 public void setup(){
   
   
   nodes = new ArrayList<Node>();
   edges = new ArrayList<Edge>();
+
 
 }
 
@@ -100,6 +105,7 @@ public void draw(){
   if(overNode != null && dist(mouseX,mouseY,overNode.x,overNode.y)>NODE_RADIUS -5){
     isOver = false;
   }
+  
 }
 
 boolean locked = false;
@@ -124,9 +130,13 @@ public void mousePressed() {
        }
        if(canAdd){
          edges.add(adgeToAdd);
+         graph[adgeToAdd.getFrom().id][adgeToAdd.getTo().id]=1;
+         graph[adgeToAdd.getTo().id][adgeToAdd.getFrom().id]=1;
          lastSelected.isSelected = false;
          lastSelected = null;
        }
+
+       
      }
     }
   }
@@ -136,7 +146,21 @@ public void mousePressed() {
   } else {
     locked = false;
   }
+  this.createMatrix();
 }
+public int[][] createMatrix() {
+
+    System.out.println("------------------");
+    for(int i = 0; i<nodes.size();i++) {
+ 	   for(int j = 0;j<nodes.size();j++) {
+ 		   System.out.print(graph[i][j] + " ");
+ 	   }
+ 	   System.out.print("\n");
+    }
+    return graph;
+}
+
+
 
 public void mouseDragged() {
   if(mouseButton == LEFT && locked) {
@@ -159,10 +183,21 @@ public void keyPressed(){
   
   if(key == 'a' && !isPressed) {
     nodes.add(new Node(mouseX,mouseY,nodes.size()));
+    graph = generateNewMatrix();
     isPressed = true;
   }
 }
-  public void settings() {  size(1280,720); }
+public int[][] generateNewMatrix() {
+	int[][] tmp = new int[graph.length+1][graph.length+1];
+	for(int i = 0; i<graph.length;i++) {
+		for(int j = 0;j<graph[0].length;j++ ) {
+			tmp[i][j] = graph[i][j];
+		}
+	}
+	return tmp;
+}
+
+  public void settings() {  size(1200,600); }
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Graph" };
     if (passedArgs != null) {
@@ -170,5 +205,6 @@ public void keyPressed(){
     } else {
       PApplet.main(appletArgs);
     }
+    
   }
 }
